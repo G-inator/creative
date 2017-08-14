6e96d190d3a762a17444af91ac7e62fcd4c8f282
Fernando <fepegar@gmail.com>
2017-08-14 02:30:48 +0100

    Remove hard-coded variables

--- circle-cone/sketch.js
+++ circle-cone/sketch.js
@@ -1,19 +1,46 @@
+var bg = 255;
+var triangleArcColor;
+var triangleColor;
+var bottomCircleColor;
+
+var triangleArcX = 250;
+var triangleArcY = 150;
+var triangleArcD = 80;
+var triangleArcStart = 0;
+var triangleArcStop;
+
 function setup() { 
-  createCanvas(640,360);
+  createCanvas(windowWidth, windowHeight);
+  triangleArcColor = color(0, 0, 255);
+  triangleColor = color(0, 250, 0);
+  bottomCircleColor = color(255, 0, 0);
+  triangleArcStop = -HALF_PI;
 } 
 
 function draw() { 
-	background(255);
+	background(bg);
   
   //arco del triangulo
-  fill(0, 0, 255)
-  arc(250,150,80,80,0,-HALF_PI, OPEN);
+  fill(triangleArcColor);
+  arc(triangleArcX, triangleArcY,
+      triangleArcD, triangleArcD,
+      0, triangleArcStop, OPEN);
   
   //triangulo
-  fill(0, 250, 0)
-	triangle(290,150,250,110,290,110);
+  fill(triangleColor);
+  var r = triangleArcD/2
+  var x1 = triangleArcX + r;
+  var y1 = triangleArcY;
+  var x2 = triangleArcX;
+  var y2 = triangleArcY - r;
+  var x3 = x1;
+  var y3 = y2;
+  triangle(x1, y1, x2, y2, x3, y3);
   
   //circulo de abajo
-  fill(255, 0, 0)
-	ellipse(193,207,80,80);
+  fill(bottomCircleColor);
+  var x = triangleArcX - triangleArcD * cos(HALF_PI/2);
+  var y = triangleArcY + triangleArcD * sin(HALF_PI/2);
+	ellipse(x, y,
+          triangleArcD, triangleArcD);
 }
